9275b508cb182cc556baa1b31dedb7093325844e
yudainemot0 <majesticsurfboard@gmail.com>
2023-03-17 12:47:15 +0900

    ✨ 各記事にリンク を追加

--- components/Article.tsx
+++ components/Article.tsx
@@ -2,14 +2,17 @@ interface Article {
   title: string;
   description: string;
   urlToImage: string;
+  url: string;
 }
 
 export default function Article({ article }: { article: Article }) {
   return (
     <li>
-      <h2>{article.title}</h2>
-      {article.urlToImage && <img src={article.urlToImage} alt="article" />}
-      <p>{article.description}</p>
+      <a href={article.url} target="_blank" rel="noopener noreferrer" className="flex flex-col gap-1">
+        {article.urlToImage && <img src={article.urlToImage} alt="article" />}
+        <h2 className=" font-bold">{article.title}</h2>
+        <p>{article.description}</p>
+      </a>
     </li>
   );
 }
