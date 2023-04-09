44cd544232740c125fde936160503e72db714fdd
yudainemot0 <majesticsurfboard@gmail.com>
2023-04-10 07:08:23 +0900

    🎨 bitcoinとethereumの最新価格取得API実装

--- components/BitcoinPrice.tsx
+++ components/BitcoinPrice.tsx
@@ -1,7 +1,30 @@
-export default function() {
-    return (
-        <div>
-            $Bitcoin
-        </div>
-    );
+import { useState, useEffect } from "react";
+
+export default function CryptPrice() {
+  const [bitcoinPrice, setBitcoinPrice] = useState<number | null>(null);
+
+  const ids = "bitcoin";
+  const vs_currencies = "usd";
+  const apiUrl = `https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=${vs_currencies}`;
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setBitcoinPrice(data.bitcoin.usd);
+      })
+      .catch((error) => console.error(error));
+  }, [apiUrl]);
+
+  return (
+    <div className="flex gap-6">
+      <div>
+        {/* bitcoin */}
+        <h1 className="text-2xl font-bold">Bitcoin</h1>
+        <h2 className="text-lg font-medium">
+          {bitcoinPrice ? `$ ${bitcoinPrice.toLocaleString()}` : "-"}
+        </h2>
+      </div>
+    </div>
+  );
 }
--- components/EthereumPrice.tsx
+++ components/EthereumPrice.tsx
@@ -1,7 +1,30 @@
-export default function() {
-    return (
-        <div>
-            $Ethereum
-        </div>
-    );
+import { useState, useEffect } from "react";
+
+export default function CryptPrice() {
+  const [ethereumPrice, setEthereumPrice] = useState<number | null>(null);
+
+  const ids = "ethereum";
+  const vs_currencies = "usd";
+  const apiUrl = `https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=${vs_currencies}`;
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setEthereumPrice(data.ethereum.usd);
+      })
+      .catch((error) => console.error(error));
+  }, [apiUrl]);
+
+  return (
+    <div className="flex gap-6">
+      <div>
+        {/* ethereum */}
+        <h1 className="text-2xl font-bold">Ethereum</h1>
+        <h2 className="text-lg font-medium">
+          {ethereumPrice ? `$ ${ethereumPrice.toLocaleString()}` : "-"}
+        </h2>
+      </div>
+    </div>
+  );
 }
