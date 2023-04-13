ef64bb1ccf6e0d12a4635c5a748c515f6ba5852d
yudainemot0 <majesticsurfboard@gmail.com>
2023-04-14 07:41:16 +0900

    ✨ headerIconに遷移先と各ページの土台作成

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,6 +10,7 @@ import {
 } from "@heroicons/react/24/outline";
 import HeaderRow from "./HeaderRow";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
@@ -22,15 +23,43 @@ export default function Header() {
         alt=""
       />
       <div className="flex justify-around items-center mt-3 mb-1 mx-auto max-w-7xl">
-        <div className="flex space-x-3 sm:space-x-6 md:space-x-10">
-          <HeaderRow Icon={GlobeAltIcon} title="TOP" />
-          <HeaderRow Icon={NewspaperIcon} title="General" />
-          <HeaderRow Icon={ComputerDesktopIcon} title="Tecnology" />
-          <HeaderRow Icon={PresentationChartLineIcon} title="Business" />
-          <HeaderRow Icon={BeakerIcon} title="Science" />
-          <HeaderRow Icon={IdentificationIcon} title="Health" />
-          <HeaderRow Icon={TrophyIcon} title="Sports" />
-        </div>
+        <ul className="flex space-x-3 sm:space-x-6 md:space-x-10">
+          <li>
+            <Link href="/">
+              <HeaderRow Icon={GlobeAltIcon} title="TOP" />
+            </Link>
+          </li>
+          <li>
+            <Link href="/General">
+              <HeaderRow Icon={NewspaperIcon} title="General" />
+            </Link>
+          </li>
+          <li>
+            <Link href="/Tecnology">
+              <HeaderRow Icon={ComputerDesktopIcon} title="Tecnology" />
+            </Link>
+          </li>
+          <li>
+            <Link href="/Business">
+              <HeaderRow Icon={PresentationChartLineIcon} title="Business" />
+            </Link>
+          </li>
+          <li>
+            <Link href="/Science">
+              <HeaderRow Icon={BeakerIcon} title="Science" />
+            </Link>
+          </li>
+          <li>
+            <Link href="/Health">
+              <HeaderRow Icon={IdentificationIcon} title="Health" />
+            </Link>
+          </li>
+          <li>
+            <Link href="/Sports">
+              <HeaderRow Icon={TrophyIcon} title="Sports" />
+            </Link>
+          </li>
+        </ul>
         <div className="flex items-center space-x-2 bg-gray-100 p-1 sm:p-3 rounded-full">
           <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" />
           <input
